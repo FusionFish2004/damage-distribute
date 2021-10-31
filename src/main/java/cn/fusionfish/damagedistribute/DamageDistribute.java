7a801242fc7e29e3eb6a99eaa2d455f8905071ef
JeremyHu <huquancheng329@gmail.com>
2021-10-31 19:33:39 +0800

    Initial Commit

--- src/main/java/cn/fusionfish/damagedistribute/DamageDistribute.java
+++ src/main/java/cn/fusionfish/damagedistribute/DamageDistribute.java
@@ -4,18 +4,32 @@ import cn.fusionfish.damagedistribute.listeners.EntityDamageByEntityListener;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.io.File;
+
 public class DamageDistribute extends JavaPlugin {
 
     private static DamageDistribute instance;
+    public final File CONFIG_FILE = new File(getDataFolder(), "config.yml");
 
     @Override
     public void onEnable() {
         instance = this;
         Bukkit.getPluginManager().registerEvents(new EntityDamageByEntityListener(), this);
+
+        if (!getDataFolder().exists()) {
+            boolean result = getDataFolder().mkdir();
+            if (!result) {
+                Bukkit.getPluginManager().disablePlugin(this);
+            }
+        }
+
+        if (!CONFIG_FILE.exists()) {
+            saveDefaultConfig();
+        }
     }
 
     public static DamageDistribute getInstance() {
         return instance;
     }
-    
+
 }
